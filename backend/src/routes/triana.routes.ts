66ec698e10f8dfeb76d0a0daaaa269004cf8e36d
evelynmunoz25 <evelynasdmunoz@gmail.com>
2020-09-15 07:41:24 -0300

    Rutas de Triana_Producto

--- backend/src/routes/triana.routes.ts
+++ backend/src/routes/triana.routes.ts
@@ -1,14 +1,14 @@
-Mimport { Router } from "express";
-import { TrianaController } from "../controllers/triana.controller";
+import { Router } from "express";
+import { TrianaController } from "../controllers/triana.controllers";
 
 let trianaController = new TrianaController();
 
 const enrutadorTriana = Router();
 
-enrutadorTriana.route('/triana').get(trianaController.listarTriana); //obtener datos y mostrarlo
-enrutadorTriana.route('/triana').post(trianaController.guardarTriana); //guardar datos en la base de datos
-enrutadorTriana.route('/triana/:codigo_triana').delete(trianaController.eliminarTriana); //elimina datos de la base de datos
-enrutadorTriana.route('/triana/:codigo_triana').put(trianaController.actualizarTriana); //actualiza datos de la base de datos
-enrutadorTriana.route('/triana/:codigo_triana').get(trianaController.obtenerUnTriana); //obtiene un valor de a bae de atos
+enrutadorTriana.route('/triana_producto').get(trianaController.listarTrianaProducto); //obtener datos y mostrarlo
+enrutadorTriana.route('/triana_producto').post(trianaController.guardarTrianaProducto); //guardar datos en la base de datos
+enrutadorTriana.route('/triana_producto/:codigo_triana_producto').delete(trianaController.eliminarTrianaProducto); //elimina datos de la base de datos
+enrutadorTriana.route('/triana_producto/:codigo_triana_producto').put(trianaController.actualizarTrianaPoducto); //actualiza datos de la base de datos
+enrutadorTriana.route('/triana_producto/:codigo_triana_producto').get(trianaController.obtenerUnTrianaProducto); //obtiene un valor de a bae de atos
 export default enrutadorTriana;
 
