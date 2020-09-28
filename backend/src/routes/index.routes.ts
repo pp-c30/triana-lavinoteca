3674067c78ac749cf983b6d7f3315b8b8d857549
lorenaancaten <lorenaancaten81@gmail.com>
2020-09-28 19:46:35 -0300

    rutas

--- backend/src/routes/index.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import {  } from "../controllers/index.controllers";
-
-
-let enrutadorIndex = Router();
-
-enrutadorIndex.route('/').get();
-export default enrutadorIndex;
