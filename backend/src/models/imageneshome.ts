95726633cbc7c8b8c3873047d337332542d1cd42
CatalinaColichelli <colichellic@cet30.edu.ar>
2020-09-28 15:41:06 -0300

    models

---
+++ backend/src/models/imageneshome.ts
@@ -0,0 +1,7 @@
+export interface IImageneshome
+{
+    id_imagen?:number;
+    nombre:string; 
+    estado:number;
+    url:number;
+}
---
+++ backend/src/models/variedad.ts
@@ -0,0 +1,5 @@
+export interface IVarie
+{
+    id_varie?:number;
+    descripcion:string; 
+}
