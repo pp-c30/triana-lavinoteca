326dba4bebd94cc7d85cecffde1116109a79086f
evelynmunoz25 <evelynasdmunoz@gmail.com>
2020-09-30 06:55:07 -0300

    lalala

--- backend/src/routes/opinion.routes.ts
+++ backend/src/routes/opinion.routes.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 import { OpinionController } from "../controllers/opinion.controllers";
 
 let trianaController = new OpinionController();
+let byb
 
 const enrutadorTriana = Router();
+
 
 enrutadorTriana.route('/triana_opinion').get(trianaController.listarTrianaOpinion); //obtener datos y mostrarlo
 enrutadorTriana.route('/triana_opinion').post(trianaController.guardarTrianaOpinion); //guardar datos en la base de datos
