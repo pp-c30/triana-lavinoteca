9007c0a84fefc564cbe7ebf00c8b1fab9949b548
lorenaancaten <lorenaancaten81@gmail.com>
2020-10-01 21:03:45 -0300

    configuracion

--- backend/src/controllers/imageneshome.controller.ts
+++ backend/src/controllers/imageneshome.controller.ts
@@ -2,13 +2,13 @@ import { conexion } from "../database";
 
 import { Request, Response } from "express";
 
-import { IImageneshome } from '../models/imageneshome';
 
 export class ImagenesHomeController{
     //operaciones CRUD 
-    listarTrianaImagenesHome(){}
-    public guardarTrianaImagenesHome(){}
-    eliminarTrianaImagenesHome(){}
-    actualizarTrianaImagenesHome(){}
-    obtenerUnTrianaImagenesHome(){}
+    public async guardarTrianaImagenesHome(req:Request, res:Response){
+        const db = await conexion();
+
+        await db.query('insert into imageneshome set ?', []);
+
+    }
 }
