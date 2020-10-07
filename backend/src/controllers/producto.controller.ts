0597c6aeb79242107bea0d628b9c9f3e9a66fa1d
lorenaancaten <lorenaancaten81@gmail.com>
2020-10-07 15:30:15 -0300

    ultimo cambio

--- backend/src/controllers/producto.controller.ts
+++ backend/src/controllers/producto.controller.ts
@@ -1,8 +1,7 @@
 import { conexion } from "../database";
-
 import { Request, Response } from "express"; 
-
-import { IProducto } from '../models/producto';
+import cloudinary from "cloudinary";
+import fs from "fs-extra";
 
 export class ProductoController{
     //Listado de producto
@@ -19,14 +18,34 @@ export class ProductoController{
     //Guardar producto
     public async guardarTrianaProducto(req:Request, res:Response)
     {
-        const db= await conexion();
+        const db = await conexion();
 
-        const triana_producto:IProducto= req.body;
+        const url_img=req.file.path;
 
-        await db.query("insert into producto set ?",[triana_producto]);
+        //fuimos a buscar la imagen a la carpeta upload para subirla a cloudinary
+        const resultado_cloud =await cloudinary.v2.uploader.upload(req.file.path);
+    
+        //se guardan datos en la base
+        const guardarImagen = {
+            nombre:req.body.nombre,
+            categoria:req.body.categoria,
+            stock:req.body.stock,
+            precio:req.body.precio,
+            imagen_url: resultado_cloud.url,
+            public_id:resultado_cloud.public_id,
+            bodega:req.body.bodega,
+            descripcion:req.body.descripcion,
+            cantmil:req.body.cantmil,
+            estado:req.body.estado
+        }
 
-        return res.json('El producto fue guardado exitosamente');
+        await db.query('insert into producto set ?', [guardarImagen]);
+
+        fs.unlink(req.file.path);
+
+        res.json('Se guardo exitosamente los datos y la imagen');
     }
+
 
     //actualizar producto
     public async actualizarTrianaPoducto(req:Request, res:Response)
--- backend/src/routes/producto.routes.ts
+++ backend/src/routes/producto.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { ProductoController } from "../controllers/producto.controller";
+import multer from "../libs/multer";
 
 let trianaController = new ProductoController();
 
 const enrutadorProducto = Router();
 
 enrutadorProducto.route('/triana_producto').get(trianaController.listarTrianaProducto); //obtener datos y mostrarlo
-enrutadorProducto.route('/triana_producto').post(trianaController.guardarTrianaProducto); //guardar datos en la base de datos
+enrutadorProducto.route('/triana_producto').post(multer.single('img'),trianaController.guardarTrianaProducto); //guardar datos en la base de datos
 enrutadorProducto.route('/triana_producto/:codigo_triana_producto').put(trianaController.actualizarTrianaPoducto); //actualiza datos de la base de datos
 enrutadorProducto.route('/triana_producto/:codigo_triana_producto').get(trianaController.obtenerUnTrianaProducto); //obtiene un valor de a bae de atos
 export default enrutadorProducto;
