bc52cdfc84022ef59bfb7ee5d1f9a407cff01986
evelynmunoz25 <evelynasdmunoz@gmail.com>
2020-11-12 08:27:57 -0300

    Guardar y Eliminar de Opinion

--- frontend/src/app/components/opiniones/opiniones.component.ts
+++ frontend/src/app/components/opiniones/opiniones.component.ts
@@ -2,15 +2,28 @@ import { Component, OnInit } from '@angular/core';
 
 import { OpinionesService } from "../../services/opiniones.service";
 
+import { FormBuilder, FormGroup, Form } from "@angular/forms";
+
 @Component({
   selector: 'app-opiniones',
   templateUrl: './opiniones.component.html',
   styleUrls: ['./opiniones.component.css']
 })
 export class OpinionesComponent implements OnInit {
+  
   listOpiniones = [];
 
-  constructor(private opinionesserv:OpinionesService) { }
+  formOpiniones: FormGroup;
+
+  constructor(private opinionesserv:OpinionesService,private fb: FormBuilder)
+   {
+    this.formOpiniones = this.fb.group({
+
+  
+      descripcion:['']
+
+    });
+   }
 
   ngOnInit(): void {
     this.obtenerOpiniones();
@@ -23,5 +36,36 @@ export class OpinionesComponent implements OnInit {
       error => console.log(error)
     )
   }
+
+  guardarOpiniones()
+  {
+    //console.log(this.formOpiniones.value);  
+    this.opinionesserv.saveOpiniones(this.formOpiniones.value).subscribe(
+      resultado => {
+        console.log(resultado);
+        //se refresca la grilla
+        this.obtenerOpiniones();
+        this.formOpiniones.reset();
+      },
+      error => console.log(error)
+    );
+
+  }
+
+  eliminarOpiniones(codigo_triana_opinion:number)
+  {
+    if(confirm('Esta seguro que desa ejecutar esta accion?')){
+      this.opinionesserv.deleteOpiniones(codigo_triana_opinion).subscribe(
+        respuesta =>{
+          console.log(respuesta);
+          this.obtenerOpiniones();
+        },
+        error => console.log(error)
+      );
+    }
+    
+  }
+
+
 
 }
--- frontend/src/app/services/opiniones.service.ts
+++ frontend/src/app/services/opiniones.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
 import { Iopinion } from "../models/Opinion";
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,7 +15,15 @@ export class OpinionesService {
    }
    getOpiniones()
     {
-      return this.http.get<Iopinion[]>('http:localhost:3000/triana_opinion')
+      return this.http.get<Iopinion[]>('http://localhost:3000/triana_opinion')
+    }
+    saveOpiniones(UnOpiniones:Iopinion)
+    {
+      return this.http.post('http://localhost:3000/triana_opinion/',UnOpiniones);
     }
 
+    deleteOpiniones(codigo_triana_opinion:number){
+      return this.http.delete('http://localhost:3000/codigo_triana_opinion'+codigo_triana_opinion);
+
+    }
 }
