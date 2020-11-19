d268d9fbb7af129ba537c76ab787decf52d8b510
lorenaancaten <lorenaancaten73@gmail.com>
2020-11-19 00:26:07 -0300

    nuevos cambios

--- frontend/src/app/components/admin-imageneshome/admin-imageneshome.component.ts
+++ frontend/src/app/components/admin-imageneshome/admin-imageneshome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageneshomeService } from '../../services/imageneshome.service';
+import { IImageneshome } from '../../../../../backend/src/models/imageneshome';
 
 @Component({
   selector: 'app-admin-imageneshome',
@@ -7,12 +8,25 @@ import { ImageneshomeService } from '../../services/imageneshome.service';
   styleUrls: ['./admin-imageneshome.component.css']
 })
 export class AdminImageneshomeComponent implements OnInit {
+
+  // tslint:disable-next-line: variable-name
+  listado_datos_imageneshome = [];
+
   // servimageneshome: es una instancia que nos permitira acceder a los metodos que contiene la clase ImageneshomeService
   constructor(private servimageneshome: ImageneshomeService) { }
 
   ngOnInit(): void {
+    this.listarImagenesHome();
   }
 
+
   listarImagenesHome()
-  {}
+  {
+    this.servimageneshome.getImagenesHome().subscribe(
+      resultado => {
+        this.listado_datos_imageneshome = resultado;
+      },
+      error => console.log(error)
+    );
+  }
 }
