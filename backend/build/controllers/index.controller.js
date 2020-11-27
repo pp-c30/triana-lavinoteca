6e3fae4742d17d8ae33c3cd990889e91cd93ec06
CatalinaColichelli <colichellic@cet30.edu.ar>
2020-11-27 01:18:22 -0300

    .

--- backend/build/controllers/index.controller.js
+++ backend/build/controllers/index.controller.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.mensaje_bienvenida = void 0;
 function mensaje_bienvenida(req, res) {
     res.json("Esta es la ruta principal de la página");
 }
